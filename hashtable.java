61baa93983efed0340f6e14256545be28d793f0f
rashmi2503524205-collab <rashmi.2503524205@vcet.edu.in>
2026-07-30 11:45:00 +0530

    Add files via upload

---
+++ hashtable.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashTable {
+
+    public static void main(String[] args) {
+
+        
+        HashMap<Integer, String> studentMap = new HashMap<>();
+        studentMap.put(38, "rashmi");
